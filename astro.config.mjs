d7f44b24513dab90665d4731f4ea72e6e913ad17
ThatXliner <bryan.hu.2020@gmail.com>
2025-11-25 16:26:53 -0800

    Back to new template

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,5 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-import icon from "astro-icon";
-
-import svelte from "@astrojs/svelte";
+// @ts-check
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-  site: "https://thatxliner.github.io",
-  base: "/resume",
-  integrations: [tailwind(), icon(), svelte()]
-});
+export default defineConfig({});
